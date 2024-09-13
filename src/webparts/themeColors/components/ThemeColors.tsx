213a79186e06a88f9bed1746ba3e2d7254e8dce3
Glenn Reian <Glenn.Reian@regionvasterbotten.se>
2024-09-13 08:56:52 +0200

    Cleaning of obsolete comments

--- src/webparts/themeColors/components/ThemeColors.tsx
+++ src/webparts/themeColors/components/ThemeColors.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import type { IThemeColorsProps } from './IThemeColorsProps';
 import { createTheme, ITheme } from "office-ui-fabric-react";
 import "office-ui-fabric-core/dist/css/fabric.min.css";
-// import * as sharedStyling from '../../../../common/styling/sharedStyling';
-
 import styles from './ThemeColors.module.scss';
 
 export default class ThemeColors extends React.Component<IThemeColorsProps, {}> {
@@ -112,46 +110,3 @@ export default class ThemeColors extends React.Component<IThemeColorsProps, {}> 
 
 
 }
-
-
-// {this.getColorBlock(palette.themePrimary, 'themePrimary')}
-// {this.getColorBlock(palette.themeLighterAlt, 'themeLighterAlt')}
-// {this.getColorBlock(palette.themeLighter, 'themeLighter')}
-// {this.getColorBlock(palette.themeLight, 'themeLight')}
-
-
-// {this.getColorBlock(palette.themeSecondary, 'themeSecondary')}
-// {this.getColorBlock(palette.themeTertiary, 'themeTertiary')}
-
-// {this.getColorBlock(palette.themeDark, 'themeDark')}
-// {this.getColorBlock(palette.themeDarkAlt, 'themeDarkAlt')}
-// {this.getColorBlock(palette.themeDarker, 'themeDarker')}
-// {this.getColorBlock(palette.themeLight, 'themeLight')}
-// {this.getColorBlock(palette.themeLighter, 'themeLighter')}
-// {this.getColorBlock(palette.themeLighterAlt, 'themeLighterAlt')}
-
-// {this.getColorBlock(palette.neutralDark, 'neutralDark')}
-// {this.getColorBlock(palette.neutralLight, 'neutralLight')}
-// {this.getColorBlock(palette.neutralLighter, 'neutralLighter')}
-// {this.getColorBlock(palette.neutralLighterAlt, 'neutralLighterAlt')}
-// {this.getColorBlock(palette.neutralPrimary, 'neutralPrimary')}
-// {this.getColorBlock(palette.neutralPrimaryAlt, 'neutralPrimaryAlt')}
-// {this.getColorBlock(palette.neutralSecondary, 'neutralSecondary')}
-// {this.getColorBlock(palette.neutralSecondaryAlt, 'neutralSecondaryAlt')}
-// {this.getColorBlock(palette.neutralTertiary, 'neutralTertiary')}
-// {this.getColorBlock(palette.neutralTertiaryAlt, 'neutralTertiaryAlt')}
-// {this.getColorBlock(palette.neutralQuaternary, 'neutralQuaternary')}
-// {this.getColorBlock(palette.neutralQuaternaryAlt, 'neutralQuaternaryAlt')}
-
-// {this.getColorBlock(palette.accent, 'accent')}
-// {this.getColorBlock(palette.red, 'red')}
-// {this.getColorBlock(palette.redDark, 'redDark')}
-// {this.getColorBlock(palette.orange, 'orange')}
-// {this.getColorBlock(palette.orangeLight, 'orangeLight')}
-// {this.getColorBlock(palette.orangeLighter, 'orangeLighter')}
-// {this.getColorBlock(palette.accent, 'accent')}
-// {this.getColorBlock(palette.green, 'green')}
-// {this.getColorBlock(palette.greenLight, 'greenLight')}
-// {this.getColorBlock(palette.greenDark, 'greenDark')}
-// {this.getColorBlock(palette.yellow, 'yellow')}
-// {this.getColorBlock(palette.yellowLight, 'yellowLight')}
